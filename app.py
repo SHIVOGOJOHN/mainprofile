99127123c892d54101a6dd581cb267bb72eb9da5
JOHN SHIVOGO <jonesjorney@gmail.com>
2025-06-27 00:46:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com' # Replace with your SMTP server
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'techbidmarketplace@gmail.com' # Replace with your email
-app.config['MAIL_PASSWORD'] = 'os.getenv("MAIL_PASSWORD") # Replace with your email password
+app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD") # Replace with your email password
 app.config['MAIL_DEFAULT_SENDER'] = 'techbidmarketplace@gmail.com' # Replace with your email
 
 mail = Mail(app)
